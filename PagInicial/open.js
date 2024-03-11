f5d71d64b9990f146f0e5154376d57e25e4ccdf4
EdvaldoDantas <135360521+EdvaldoDantas@users.noreply.github.com>
2024-03-11 20:16:05 +0100

    up

--- PagInicial/open.js
+++ PagInicial/open.js
@@ -1,31 +1,37 @@
 let menu_escondido = document.getElementById('menu-escondido')
 let menu = document.getElementById('menu')
 let fechado = true
-
-menu.addEventListener('click',()=>{
+ 
+document.addEventListener('DOMContentLoaded',()=>{
+    menu.addEventListener('click',()=>{
    
-    if(menu_escondido.classList.contains('fechado')){
-        menu_escondido.classList.remove('fechado')
-        menu_escondido.classList.add('abrir')
-    }
-    else if(menu_escondido.classList.contains('abrir')){
-        menu_escondido.classList.remove('abrir')
-        menu_escondido.classList.add('fechado')
-    }
+        if(menu_escondido.classList.contains('inicio') | menu_escondido.classList.contains('fechado')){
+            menu_escondido.classList.remove('inicio')
+            menu_escondido.classList.remove('fechado')
+            menu_escondido.classList.add('abrir')
+        }
+        else if(menu_escondido.classList.contains('abrir')){
+            menu_escondido.classList.remove('abrir')
+            menu_escondido.classList.add('fechado')
+        }
+    })
+    document.addEventListener('click',(event)=>{
+        const cli = menu.contains(event.target)
+        if(!cli){
+             menu_escondido.classList.remove('abrir')
+             menu_escondido.classList.add('inicio')
+        }
+     })
+     document.addEventListener('scroll',()=>{
+        if(!menu_escondido.classList.contains('inicio')){
+             menu_escondido.classList.remove('abrir')
+             menu_escondido.classList.add('fechado')
+        }
+    })
 })
 
 definicao = document.getElementById("Def")
 definicao.addEventListener('click',()=>{
     window.location.href = 'Definições.html'
 })
-document.addEventListener('click',(event)=>{
-   const cli = menu.contains(event.target)
-   if(!cli){
-        menu_escondido.classList.remove('abrir')
-        menu_escondido.classList.add('fechado')
-   }
-})
-document.addEventListener('scroll',()=>{
-    menu_escondido.classList.remove('abrir')
-    menu_escondido.classList.add('fechado')
-})
+
