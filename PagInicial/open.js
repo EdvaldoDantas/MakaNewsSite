ac6c66ff50e50b373fb13ba4a17e9ce41dd43a2c
EdvaldoDantas <135360521+EdvaldoDantas@users.noreply.github.com>
2024-03-13 02:14:28 +0100

    up

--- PagInicial/open.js
+++ PagInicial/open.js
@@ -5,7 +5,8 @@ let fechado = true
 let aberto = false
 
 let firstPosition = ()=>{
-    nav_menu.style ="position: absolute; right:-159px"
+    nav_menu.style ="position: absolute; right:-173px"
+    nav_menu.style.transition = 'right 0.5s ease'
 }
 var posicao = parseInt(nav_menu.style.right)
 window.onload = firstPosition()
@@ -13,23 +14,24 @@ window.onload = firstPosition()
     menu.addEventListener('click',()=>{
         let posicao = parseInt(nav_menu.style.right)
         if(fechado){
-            posicao = -10
+            posicao = -12
             nav_menu.style.right = `${posicao}px`
             fechado = false
             aberto = true
         }
         else if(aberto){
-            posicao = -159
+            posicao = -173
             nav_menu.style.right = `${posicao}px` 
             fechado = true
             aberto = false
         }
     })
+
 document.addEventListener('click',(event)=>{
-     let cont = menu.contains(event.target)
+    let cont = menu.contains(event.target)
     if(!cont){
         if(aberto){
-            posicao = -159
+            posicao = -173
             nav_menu.style.right = `${posicao}px` 
             fechado = true
             aberto = false
@@ -38,7 +40,7 @@ document.addEventListener('click',(event)=>{
 })
 document.addEventListener('scroll',()=>{
     if(aberto){
-        posicao = -159
+        posicao = -173
         nav_menu.style.right = `${posicao}px` 
         fechado = true
         aberto = false
