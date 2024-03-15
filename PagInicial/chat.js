9ef45a32feb396d25188430f3bdf4d8a772c22c9
EdvaldoDantas <135360521+EdvaldoDantas@users.noreply.github.com>
2024-03-15 23:00:31 +0100

    up

--- PagInicial/chat.js
+++ PagInicial/chat.js
@@ -1,3 +1,8 @@
 function BackHome(){
     window.location.href = 'index.html'
 }
+let div_grupo = document.querySelector('.div-grupo')
+
+div_grupo.addEventListener('click',()=> {
+    window.location.href = 'grupos.html'
+})
